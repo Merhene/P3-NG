55a12a79a0c6351313ec6a6c83fe2a8b8a4145e2
Merhene <merwan.st@gmail.com>
2024-02-29 18:51:55 +0100

    commit for the third Angular quest

--- src/app/block/block.component.ts
+++ src/app/block/block.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-block',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './block.component.html',
   styleUrl: './block.component.scss'
 })
+
 export class BlockComponent {
+
+  name: string = 'Bob';
+  imgSrc: string = "https://via.placeholder.com/350x150";
+
+  sayHello(): void {
+    alert("Bonjour !");
+  }
 
 }
