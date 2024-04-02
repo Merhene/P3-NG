f5532e517245d760c9ab447e99923f27657f6330
Merhene <merwan.st@gmail.com>
2024-04-02 07:41:16 +0200

    quest 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BlockComponent } from './block/block.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { CatalogComponent } from './catalog/catalog.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BlockComponent, UserProfileComponent],
+  imports: [RouterOutlet, BlockComponent, UserProfileComponent, CatalogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
